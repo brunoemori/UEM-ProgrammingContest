3dfadc2187b5abe6a66f9454356a9dbd2282545f
Bruno Emori <bruno.emori@gmail.com>
2018-03-12 20:27:14 -0300

    Minor changes

--- signup/views.py
+++ signup/views.py
@@ -9,12 +9,12 @@ def signup(request):
             user = form.save(commit=False)
             user.firstName = form.cleaned_data.get('first_name')
             user.lastName = form.cleaned_data.get('last_name')
-
-            image = form.clean_image()
-
+            
+            image = form.cleaned_data.get('avatar')
+                
             if (image):
                 user.avatar = request.FILES['avatar']
-
+    
             user.save()
                 
             return redirect('/')
